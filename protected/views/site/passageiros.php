8d74167fa159481bef62b3dcf53be3829526395a
Mateus Fernandes <mateusinho.fernandes@aluno.uece.br>
2025-02-04 09:45:40 -0300

    conexao com banco

--- protected/views/site/passageiros.php
+++ protected/views/site/passageiros.php
@@ -2,7 +2,26 @@
 /* @var $this SiteController */
 
 $this->pageTitle=Yii::app()->name . ' - Passageiros';
+$this->breadcrumbs=array(
+	'Listar passageiros',
+);
 ?>
-<h1>Passageiros</h1>
+<h1>Listar passageiros</h1>
 
 <p>Tela para visualização dos passageiros.</p>
+
+
+    <?php
+        $newSQL = "SELECT * FROM passageiro";
+		$connection = new CDbConnection('mysql:host=localhost;dbname=tabela','root','root');
+		$command = $connection->createCommand($newSQL);
+		$results = $command->queryAll();
+    ?>
+
+    <?php
+    	// Display the result
+		foreach($results as $result) {?>
+    		<li><?php echo "Database connection is working! Result: " . $result['nome'] . "<br>";?></li>
+        <?php	}
+    ?>
+
